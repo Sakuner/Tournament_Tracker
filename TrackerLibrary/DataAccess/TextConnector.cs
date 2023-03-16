090ae54e1fc4b17493d6a1ea4007a11b897e5b14
Kuba Wojciechowski <kuba99wojciechowski@gmail.com>
2023-03-16 14:21:47 +0100

    Added method that returns full file path

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -8,10 +8,15 @@ namespace TrackerLibrary
 {
     public class TextConnector : IDataConnection
     {
+        //TODO make the create prize actually do something
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            //TODO make the create prize actually do something
-            model.Id = 1;
+            // load the text file
+            // convert the text to list<prizemodel>
+            //find the ID
+            //add the new record with the new id
+            //convert the prizes to list<string>
+            //save the list<string> to the text file
             return model;
         }
     }
