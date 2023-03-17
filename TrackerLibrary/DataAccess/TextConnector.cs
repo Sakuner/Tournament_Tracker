ff17e9d783e5c25324b69c33da492b3929bda26a
Kuba Wojciechowski <kuba99wojciechowski@gmail.com>
2023-03-17 16:33:17 +0100

    saving teams to text file

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -11,6 +11,7 @@ namespace TrackerLibrary
     {
         private const string PrizesFile = "PrizeModels.csv";
         private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv";
 
         public PersonModel CreatePerson(PersonModel model)
         {
@@ -54,7 +55,18 @@ namespace TrackerLibrary
 
         public TeamModel CreateTeam(TeamModel model)
         {
-            throw new NotImplementedException();
+            List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            int currentId = 1;
+            if (teams.Count > 0)
+            {
+                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
+            teams.Add(model);
+            teams.SaveToTeamFile(TeamFile);
+            return model;
         }
 
         public List<PersonModel> GetPerson_All()
